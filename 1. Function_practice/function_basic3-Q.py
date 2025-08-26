ccfac0392e0d5f9851c0bea98fd83e50550b2243
scotty4voodoo <food.for.the.god@gmail.com@gmail.com>
2025-08-26 22:04:19 +0900

    add Q3

---
+++ 1. Function_practice/function_basic3-Q.py
@@ -0,0 +1,10 @@
+##Q1. 주어진 문자열에서 모음의 개수를 세는 함수를 count_vowel() 로 작성하세요
+
+str1 = "Hello World, it is a good time to study python"
+str2 = "Let's finish in short!"
+str3 = "And get some fun time!"
+
+## 함수 호출
+# count_vowel(str1)
+# count_vowel(str2)
+# count_vowel(str3)
